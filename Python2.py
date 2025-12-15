be4d99afd7cd0c680f369d6f535ee34a37de26e8
Shreshtha731 <kumarshreshtha731@gmail.com>
2025-12-15 15:13:50 +0000

    Sum upto 50 digits

--- Python2.py
+++ Python2.py
@@ -1,6 +1,6 @@
 lowercase=1
 uppercase=50
 sum=0
-for i in range(lowercase,uppercase):
+for i in range(lowercase,uppercase+1):
     sum=sum+i
 print(f"The sum of the numbers from {lowercase} to {uppercase} is:{sum}")    
