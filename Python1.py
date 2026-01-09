a517f066263c3816961f94d737184191ff69f3fa
Shreshtha731 <kumarshreshtha731@gmail.com>
2026-01-09 18:37:43 +0000

    File handling t1

--- Python1.py
+++ Python1.py
@@ -1,7 +1,9 @@
-def factorial():
-    prod=1
-    num=int(input("Enter a number:"))
-    for i in range (num,0,-1):
-        prod=prod*i
-    print(prod)    
-factorial()    
+
+try:
+  with open("sample.txt",'rt') as fh:
+     count=1
+     for line in fh:
+       print(f"line{count}:{line}")
+       count=count+1
+except FileNotFoundError :
+    print("Error: The file  was not found")   
