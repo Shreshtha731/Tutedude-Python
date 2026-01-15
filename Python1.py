db6f8f42a4ab4073ed5677aac0a8a6d6bff752be
Shreshtha731 <kumarshreshtha731@gmail.com>
2026-01-15 17:28:54 +0000

    Dict Stu

--- Python1.py
+++ Python1.py
@@ -1,9 +1,8 @@
-
-try:
-  with open("sample.txt",'rt') as fh:
-     count=1
-     for line in fh:
-       print(f"line{count}:{line}")
-       count=count+1
-except FileNotFoundError :
-    print("Error: The file  was not found")   
+names=("John","Adam","Alice")
+marks=(34,56,23)
+data=dict(zip(names,marks))
+ask=input("Enter the student's name:")
+if ask in data:
+  print(f"{ask}'s marks is {data[ask]}:")
+else:
+  print("Student  not found")  
